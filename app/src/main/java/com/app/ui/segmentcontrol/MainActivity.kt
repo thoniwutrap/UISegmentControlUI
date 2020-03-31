57e1f5384619000f25167b41ff583f9fed9c5bf7
ThoniwutR <thoniwut.r.dev@gmail.com>
2020-03-31 13:43:28 +0700

    change lib

--- app/src/main/java/com/app/ui/segmentcontrol/MainActivity.kt
+++ app/src/main/java/com/app/ui/segmentcontrol/MainActivity.kt
@@ -2,6 +2,7 @@ package com.app.ui.segmentcontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
